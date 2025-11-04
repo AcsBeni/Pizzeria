938f8945347a17ef8ce7c97af9f543977ce26742
Acsbeni <acsbenjamin@turr.hu>
2025-11-04 11:49:10 +0100

    pizza

---
+++ src/app/components/system/pizzalist/pizzalist.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pizzalist',
+  imports: [],
+  templateUrl: './pizzalist.html',
+  styleUrl: './pizzalist.scss',
+})
+export class Pizzalist {
+
+}
