126f56bfc419fc43ccc7b72ddc0c22482dfdf976
Acsbeni <acsbenjamin@turr.hu>
2025-11-05 10:35:13 +0100

    profilkezelés hf

--- src/app/components/admin/pizzas/pizzas.ts
+++ src/app/components/admin/pizzas/pizzas.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Apiservice } from '../../../services/api.service';
+import { Pizza } from '../../../interfaces/pizza';
+import { enviroment } from '../../../../enviroment/enviroment';
 
 @Component({
   selector: 'app-pizzas',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './pizzas.html',
   styleUrl: './pizzas.scss',
 })
-export class Pizzas {
+export class Pizzas implements OnInit {
+
+  currency = enviroment.currency
+  pizzas:Pizza[]=[]
+  
+  constructor(
+    private api: Apiservice
+  ){}
+
+  ngOnInit(): void{
+    this.getPizzas();
+  }
+  getPizzas(){
+    this.api.selectAll('pizzas').then(res=>{
+      this.pizzas=res.data
+    })
+  }
 
 }
---
+++ src/app/interfaces/pizza.ts
@@ -0,0 +1,8 @@
+export interface Pizza{
+    id:number;
+    name:string;
+    description:string;
+    image:string;
+    calory:number;
+    price:number;
+}
